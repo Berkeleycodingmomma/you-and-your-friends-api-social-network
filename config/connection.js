3461040da522beba16a95a6ff69a1fdac15c483f
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-19 23:58:11 -0600

    added comment to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
+//Below importing the mongoose library
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:27017/Thomas&Friends',{
+//connecting the MongoDB using MongoDB URI provided in the environment variables/using the default URI in the environment if not set
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:3001/You&YourFriends',{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
 });
 
+//Below we are exporting the connection to the db as a module
 module.exports = mongoose.connection
